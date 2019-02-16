2b1d0a34506019a5da4b1112105e1461544cf3c9
cm718 <cm718@icloud.com>
2019-02-16 15:58:44 -0600

    app logic that displays the article titles and links on the page

---
+++ Public/app.js
@@ -0,0 +1,17 @@
+
+function displayArticles(articles) {
+    $("tbody").empty();
+
+    articles.forEach(article => {
+        let href = article.link;
+        const tr = $("<tr>").append(
+            $("<td>").text(article.title),
+            $(`<a href='${href}'>${href}</a>`)
+        );
+        $("tbody").append(tr);
+    });
+}
+
+$.getJSON("/all", function(data){
+    displayArticles(data);
+});
