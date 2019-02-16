31e082a23663f03e93e75780c43ced9abd568451
cm718 <cm718@icloud.com>
2019-02-16 15:58:16 -0600

    created the server

---
+++ server.js
@@ -0,0 +1,67 @@
+// Dependencies
+const express = require("express");
+const mongojs = require("mongojs");
+// Require axios and cheerio for scraping 
+const axios = require("axios");
+const cheerio = require("cheerio");
+// Initialize Express
+const app = express();
+app.use(express.static("public"));
+// Database configuration
+const databaseUrl = "scraper";
+const collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", function (error) {
+  console.log("Database Error:", error);
+});
+
+// Route to retrieve data from mongo and display it
+app.get("/", function (req, res) {
+  // Query the mongodb for my scraped data and return it as json object
+  db.scrapedData.find({}, function (error, articles) {
+    // Log any errors 
+    if (error) {
+      console.log(error);
+    }
+    // Send the result to the browser
+    else {
+      res.json(articles);
+    }
+  });
+});
+
+// Route 2
+// =======
+// When you visit this route, the server will scrape data from the site and save to mongo
+app.get("/scrape", function (req, res) {
+  // First remove the data from the database so it is not duplicated
+  db.scrapedData.remove();
+  // Make an axios call to the website
+  axios.get("https://www.nytimes.com").then(function (res) {
+    let $ = cheerio.load(res.data);
+    let results = [];
+    // Loop through each article parent element for the title and link
+    $("article").each(function (i, element) {
+      let title = $(element).find("h2").text();
+      let link = "www.nytimes.com";
+      link += $(element).find("a").attr("href");
+      
+      // Insert the new elements into the scraperDB
+      db.scrapedData.insert({
+        title: title,
+        link: link
+      });
+    });
+  }).then(function(){
+    // Checking for functionality
+    res.send("hey");
+  });
+});
+
+// Listen on port 3000
+app.listen(3000, function () {
+  console.log(`App running on port 3000!`);
+});
