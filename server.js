28b0ce8b15e63fd40c27076b58fdfb34cc326925
cm718 <cm718@icloud.com>
2019-03-02 14:16:48 -0600

    updating port and connections

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 
-// Initialize Express
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express and set up middleware
 const app = express();
 app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Hook mongojs configuration to the db variable
 
-mongoose.connect('mongodb://localhost:27017/Article', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Article', {useNewUrlParser: true});
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
