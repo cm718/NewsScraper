14f578f7079fbe7ee75b99c9235b6f768c445451
cm718 <cm718@icloud.com>
2019-03-02 14:26:34 -0600

    fixed port issue

--- server.js
+++ server.js
@@ -2,25 +2,23 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
-
-var PORT = process.env.PORT || 3000;
-
-// Initialize Express and set up middleware
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+// Set up middleware
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-// Hook mongojs configuration to the db variable
-
+// Hook mongoose database connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Article', {useNewUrlParser: true});
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 // Listen on port 3000
-app.listen(3000, function () {
-  console.log(`App running on port 3000!`);
+app.listen(PORT, function () {
+  console.log(`App running on port ${PORT}`);
 });
