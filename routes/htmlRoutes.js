661a2a3dca342478fd0b055aecdeff00c9eb9692
cm718 <cm718@icloud.com>
2019-03-02 14:17:30 -0600

    fixed class vs id issue

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,32 +2,33 @@ const db = require('../models');
 
 module.exports = function (app) {
 
-    app.get('/', function (req, res) {
-        db.Article.find({}, (error, dbArticle) => {
-          if (error) {
-            console.log(error);
-          }
-          console.log(dbArticle)
-          // res.send(dbArticle)
-          res.render('index', { articles: dbArticle });
-        })
-    
+  app.get('/', function (req, res) {
+    db.Article.find({}, (error, dbArticle) => {
+      if (error) {
+        console.log(error);
+      }
+      console.log(dbArticle)
+      // res.send(dbArticle)
+      res.render('index', {
+        articles: dbArticle
       });
+    })
 
-    app.get('/saved', function (req, res) {
-        db.Article.find({saved: true}, (error, savedArticle) => {
-          if (error) {
-            console.log(error);
-          }
-          console.log(savedArticle)
-          // res.send(savedArticle)
-          res.render('saved', { articles: dbArticle });
-        })
-    
+  });
+
+  app.get('/saved', function (req, res) {
+    db.Article.find({
+      saved: true
+    }, (error, savedArticle) => {
+      if (error) {
+        console.log(error);
+      }
+      console.log(savedArticle)
+      // res.send(savedArticle)
+      res.render('saved', {
+        articles: savedArticle
       });
+    })
 
-      
-
-
-
+  });
 }
