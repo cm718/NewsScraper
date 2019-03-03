9ea93c9c7880407260ab8f00d9b61177e2be68c4
cm718 <cm718@icloud.com>
2019-03-02 21:13:00 -0600

    click events added

--- Public/app.js
+++ Public/app.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
 $(document).on('click', '#scrape', () => {
     $.ajax({
         method: 'GET',
-        url: `/scrape`
+        url: `/api/scrape`
     }).then(function (data) {
         console.log(data);
         document.location.href = '/';
@@ -17,25 +17,44 @@ $(document).on('click', '#scrape', () => {
     })
 });
 
-// click event to go to the saved articles page
-$('.saveArticle').on('click', function() {
-    $(this).toggleClass("saveButtonColor");
-    // $.ajax({
-    //     method: 'PUT',
-    //     url: '/saved',
-    //     data:data
-    // }).then(function () {
-    //     document.location.href = '/saved';
-
-    // })
-});
-// click event to go to the saved articles page
-$(document).on('click', '#saved', () => {
+// click event to unsave an article
+$(document).on('click', '.saved', function() {
+    $(this).removeClass("saved");
+    var data = $(this).data("id");
+    console.log($(this).data("id"));
     $.ajax({
-        method: 'GET',
-        url: '/saved'
+        method: 'PUT',
+        url: '/api/saved',
+        data: {id: data,
+                saved: false}
     }).then(function () {
-        document.location.href = '/saved';
+        document.location.href = '/';
 
     })
+});
+
+// click event to save an article
+$(document).on('click', '.notSaved', function() {
+    $(this).addClass("saved");
+    var data = $(this).data("id");
+    console.log($(this).data("id"));
+    $.ajax({
+        method: 'PUT',
+        url: '/api/saved',
+        data: {id: data,
+                saved: true}
+    }).then(function () {
+        document.location.href = '/';
+
+    })
+});
+
+// click event to go to the saved articles page
+$(document).on('click', '#saved', () => {
+    document.location.href = '/saved';
+});
+
+// click event to go back to the home page
+$(document).on('click', '#backButton', () => {
+    document.location.href = '/';
 });
